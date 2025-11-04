b5cd3457d2c6d0b088479cb9244e81c65018e4bf
sina karimi <sinak5081@gmail.com>
2025-11-04 08:06:36 -0800

    Printing student number, year of entry, grades, and course code using the function and parameter sending combination

---
+++ Practice/Print student number, year of entry, grades and course code.py
@@ -0,0 +1,6 @@
+def mixed(stno , year,*grade,**course):
+    print("stno=",stno)
+    print("year=",year)
+    print("grade=",grade)
+    print("course=",course)
+mixed(100,98,12,14,19,course="math",course2 = "prog",cousre3 = "Data")
