db80311630060aae3eb407df5722dea514791db5
sina karimi <sinak5081@gmail.com>
2025-11-06 01:33:55 -0800

    Calculating the area of ​​rectangles, triangles, and cubes using functions and modules

---
+++ Practice/Module/Module1.py
@@ -0,0 +1,6 @@
+def rectangle(l,w):
+    return l*w
+def triangle(b,h):
+    return b*h/2
+def cube(l,w,h):
+    return l*w*h
---
+++ Practice/Module/Using a module in a function.py
@@ -0,0 +1,8 @@
+#import module
+import Module1
+length = 10
+width = 5
+height = 2
+print("area of rectangle:",Module1.rectangle(length,width))
+print("area of triangle: ",Module1.triangle(width,height))
+print("area of cube:",Module1.cube(length,width,height))
