0b1f81016b506e265468de700b447e155197d40d
sina karimi <sinak5081@gmail.com>
2025-11-08 06:08:28 -0800

    Calculating the largest number with a function and printing it in the module output

---
+++ Small projects/max_module.py
@@ -0,0 +1,5 @@
+import module1
+num1 = 23
+num2 = 21
+num3 = 24
+print("max number is: ",module1.max(num1,num2,num3))
---
+++ Small projects/max_num.py
@@ -0,0 +1,7 @@
+def max(num1,num2,num3):
+    max=num1
+    if num2>max:
+        max = num2    
+    elif num3>max:
+        max=num3
+    return max
