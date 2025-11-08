38a707cf3465060996a9b3a5e97f8eb112a6f106
sina karimi <sinak5081@gmail.com>
2025-11-08 06:18:25 -0800

    Calculates whether the first parameter is a multiple of the second parameter and prints the result in the module output.

---
+++ Small projects/Confusing.py
@@ -0,0 +1,6 @@
+def Confusing(num1,num2):
+    if(num2%num1==0):
+        result="true"
+    else:
+        result = "false"
+    return result
---
+++ Small projects/Confusing_module.py
@@ -0,0 +1,4 @@
+import Confusing
+print("result is of number 2,4: ",Confusing.Confusing(num1=2,num2=4))
+print("result is of number 12.24: ",Confusing.Confusing(num1=12,num2=24))
+print("result is of number 6,36: ",Confusing.Confusing(num1=7,num2=36))
